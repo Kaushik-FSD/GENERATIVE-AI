d2b04f492e9c34b775695eb6aabc150ad85e2a9b
KAUSHIK MANDAL <64838130+Kaushik-FSD@users.noreply.github.com>
2026-04-22 00:30:24 +0530

    Added both model class and init_chat_model implementation

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -3,14 +3,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
-from langchain_groq import ChatGroq
 
-model = ChatGroq(
-    model="llama-3.1-8b-instant",   # fast + free tier friendly
-    temperature=0
+# USING Model Class
+
+# from langchain_groq import ChatGroq
+# model = ChatGroq(
+#     model="llama-3.1-8b-instant",   # fast + free tier friendly
+#     temperature=0
+# )
+
+# response = model.invoke("What is cricket?")
+# print(response.content)
+
+# USING init_chat_model -> this is not availaible in the langChain documentation
+
+from langchain.chat_models import init_chat_model
+model = init_chat_model(
+    "groq:llama-3.1-8b-instant"
 )
-
-# print(f"LLM :: {llm}")
 
 response = model.invoke("What is cricket?")
 print(response.content)
